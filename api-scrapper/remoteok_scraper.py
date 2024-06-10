68b241549730b0d92ede4b4d09ffb372319912f1
dhirendraxd <dhirendraxd@gmail.com>
2024-06-10 10:43:13 +0545

    new emial and id to be initalized Lremains

--- api-scrapper/remoteok_scraper.py
+++ api-scrapper/remoteok_scraper.py
@@ -8,12 +8,63 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
+#https://myaccount.google.com/lesssecureapps
 BASE_URL = 'https://remoteok.com/api/'
-
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+REQUEST_HEADER = {
+    'User-Agent' : USER_AGENT,
+    'Accept-Language':'en-US, en;q=0.5',
+    
+}
 
 def get_job_posting():
-    res = requests.get(url=BASE_URL,)
+    res = requests.get(url=BASE_URL,headers=REQUEST_HEADER)
+    return res.json()
+
+def output_job_to_xls(data):
+    wb = Workbook()
+    job_sheet = wb.add_sheet('jobs')
+    headers = list(data[0].keys())
+    # print(headers)
+    for i in range(0, len(headers)):
+        job_sheet.write(0, i, headers[i])
+    for i in range(0, len(data)):
+        job = data[i]
+        values = list(job.values())
+        for x in range(0, len(values)):
+         job_sheet.write(i+1, x, values[x])
+    wb.save('remote_jobs.xls')
+
+def send_email(send_from, send_to, subject, text, files=None):
+    assert isinstance(send_to,list)
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(fil.read(), Name=basename(f))
+        part['content-disposition'] = f'attachment; filename="{basename}"'
+        msg.attach(part)
+
+    smtp = smtplib.SMTP('smtp.gmail.com: 587')
+    smtp.starttls()
+    smtp.login(send_from, '#')   #TO BE DONE LATER CREATING A EMAIL LESS SECURE AND THEN ADDING THE MAIL ID AND PAS IN IT 
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
+
+
+
 
 if __name__ == "__main__":
-    print("hello how you doin ")
+    # print("hello how you doin ")
+    json = get_job_posting()[1:]
+    output_job_to_xls(json)
+    # print(get_job_posting())
     #output hello how you doin 
+    
+    send_email('#',['dhiredhami@gmail.com'],'jobs posting', 'please find the attach of the list below ', files=['remote_jobs.xls'])
